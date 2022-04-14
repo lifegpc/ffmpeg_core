12a5350e586e1579273cee681fa93f1a7aa574c6
lifegpc <g1710431395@gmail.com>
2022-04-15 07:36:28 +0800

    Fix syntax error

--- scripts/build_ffmpeg_core_mingw-x64-debug.sh
+++ scripts/build_ffmpeg_core_mingw-x64-debug.sh
@@ -2,12 +2,12 @@ export PREFIX=`pwd`/clib
 export INSTALL_PREFIX=`pwd`/lib
 export PKG_CONFIG_PATH=$PREFIX:$PKG_CONFIG_PATH
 mkdir -p build && cd build || exit 1
-cmake \ 
-    -G "MSYS Makefiles" \ 
-    -DCMAKE_BUILD_TYPE=Debug \ 
-    -DCMAKE_PREFIX_PATH=$PREFIX \ 
-    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \ 
-    -DENABLE_WASAPI=OFF \ 
+cmake \
+    -G "MSYS Makefiles" \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
+    -DENABLE_WASAPI=OFF \
     ../ || exit 1
 make -j8 || exit 1
 make -j8 install || exit 1
