2a2489ac50aef90fcde19fd89d8bf8e649258814
lifegpc <g1710431395@gmail.com>
2022-04-26 10:43:54 +0800

    add bzlib

---
+++ scripts/build_bzlib_msvc-x64.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+SETLOCAL
+SET TOP=%CD%
+SET PREFIX=%CD%\clib
+IF NOT EXIST cbuild (
+    MD cbuild || EXIT /B 1
+)
+CD cbuild || EXIT /B 1
+git clone https://github.com/lifegpc/bzip2-msvc || EXIT /B %ERRORLEVEL%
+CD bzip2-msvc || EXIT /B 1
+IF NOT EXIST build (
+    MD build || EXIT /B 1
+)
+CD build || EXIT /B 1
+cmake ^
+    -DCMAKE_PREFIX_PATH=%PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+    ../ || EXIT /B %ERRORLEVEL%
+cmake --build . --config Release && cmake --build . --config Release --target INSTALL ^
+    || cmake --build . --config Release && cmake --build . --config Release --target INSTALL ^
+    || EXIT /B %ERRORLEVEL%
+ENDLOCAL
