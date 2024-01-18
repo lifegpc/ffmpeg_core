7e7d72ee1b9aad20082f222744171f290175a9e8
lifegpc <root@lifegpc.com>
2024-01-18 18:12:57 +0800

    CI: Use Releease version of SDL

--- scripts/build_SDL2_mingw-x64.sh
+++ scripts/build_SDL2_mingw-x64.sh
@@ -1,7 +1,8 @@
 export PREFIX=`pwd`/clib
 mkdir -p cbuild && cd cbuild || exit 1
-git clone --depth 1 https://github.com/libsdl-org/SDL || exit 1
-cd SDL && mkdir -p build && cd build || exit 1
+curl -L "https://github.com/libsdl-org/SDL/releases/download/release-2.28.5/SDL2-2.28.5.tar.gz" -o SDL2.tar.gz || exit 1
+tar -xzvf SDL2.tar.gz || exit 1
+cd SDL2-* && mkdir -p build && cd build || exit 1
 cmake \
     -G "MSYS Makefiles" \
     -DCMAKE_BUILD_TYPE=Release \
--- scripts/build_SDL2_msvc-x64.bat
+++ scripts/build_SDL2_msvc-x64.bat
@@ -6,8 +6,9 @@ IF NOT EXIST cbuild (
     MD cbuild || EXIT /B 1
 )
 CD cbuild || EXIT /B 1
-git clone --depth 1 https://github.com/libsdl-org/SDL || EXIT /B %ERRORLEVEL%
-CD SDL || EXIT /B 1
+curl -L "https://github.com/libsdl-org/SDL/releases/download/release-2.28.5/SDL2-2.28.5.tar.gz" -o SDL2.tar.gz || EXIT /B %ERRORLEVEL%
+tar -xzvf SDL2.tar.gz || EXIT /B %ERRORLEVEL%
+CD SDL2-* || EXIT /B 1
 IF NOT EXIST build (
     MD build || EXIT /B 1
 )
